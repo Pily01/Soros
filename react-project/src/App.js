48527999bc88026bf78ae4c04240a75a900f0928
Arsamat <67019552+Arsamat@users.noreply.github.com>
2023-02-13 15:06:50 -0500

    Initialized react project

---
+++ react-project/src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h1>Welcome to Soros</h1>
+      <h2>Team</h2>
+      <ul>
+        <li>Maria</li>
+        <li>Tara</li>
+        <li>Hannah</li>
+        <li>Azamat</li>
+      </ul>
+    </div>
+  );
+}
+
+export default App;
