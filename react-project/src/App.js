82a55508cd8f2ffcad11c648ea9e5b288ff49c06
Dominguez <pilymdm@bu.edu>
2023-02-26 17:00:59 -0600

    feat: add dynamic route for companies

--- react-project/src/App.js
+++ react-project/src/App.js
@@ -3,11 +3,13 @@ import {Routes, Route} from 'react-router-dom'
 import './App.css';
 
 import Home from './routes/home/home.component';
+import Company from './components/cardlist/company/company.component';
 
 const App = () => {
   return (
     <Routes>
       <Route path='/' element={<Home/>}/>
+      <Route path='/companies/:name' element={<Company/>}/>
     </Routes>
     
   );
