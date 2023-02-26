e5b0f2a3698c4f77b50d3d08df3d75f2b632d513
Dominguez <pilymdm@bu.edu>
2023-02-26 16:10:03 -0600

    feat: remove unnecesary blank spaces

--- react-project/src/data/companies.js
+++ react-project/src/data/companies.js
@@ -1,33 +1,91 @@
 let companies_list = [
-    { name: 'ABC Corporation', location: 'New York' },
-    { name: 'XYZ Inc.', location: 'Los Angeles' },
-    { name: 'ACME Co.', location: 'Chicago' },
-    { name: 'Global Industries', location: 'Miami' },
-    { name: 'Bright Future Enterprises', location: 'Dallas' },
-    { name: 'Innovative Solutions', location: 'San Francisco' },
-    { name: 'MegaCorp', location: 'Boston' },
-    { name: 'PrimeTime Corporation', location: 'Atlanta' },
-    { name: 'Northern Lights Co.', location: 'Seattle' },
-    { name: 'TechWise Solutions', location: 'Austin' },
-    { name: 'SkyHigh Enterprises', location: 'Denver' },
-    { name: 'Dynamic Industries', location: 'Washington D.C.' },
-    { name: 'Growth Company', location: 'Houston' },
-    { name: 'Pinnacle Inc.', location: 'Philadelphia' },
-    { name: 'Leading Edge Solutions', location: 'San Diego' },
-    { name: 'Summit Enterprises', location: 'Minneapolis' },
-    { name: 'Elite Industries', location: 'Phoenix' },
-    { name: 'Golden Gate Corporation', location: 'San Francisco' },
-    { name: 'Optimum Solutions', location: 'Orlando' },
-    { name: 'Bayside Corporation', location: 'Tampa' },
-    { name: 'Silver State Enterprises', location: 'Las Vegas' },
-    { name: 'First Rate Co.', location: 'New York' },
-    { name: 'Sunset Corporation', location: 'Los Angeles' },
-    { name: 'Infinite Solutions', location: 'Chicago' },
-    { name: 'East Coast Enterprises', location: 'Miami' },
-    { name: 'Rising Star Co.', location: 'Dallas' },
-    { name: 'NextGen Industries', location: 'San Francisco' },
-    { name: 'Peak Performance Corporation', location: 'Boston' },
-    { name: 'New Horizons Solutions', location: 'Atlanta' }
+    { 
+        name: 'ABC Corporation', 
+        location: 'New York' },
+    { 
+        name: 'XYZ Inc.', 
+        location: 'Los Angeles' },
+    { 
+        name: 'ACME Co.', 
+        location: 'Chicago' },
+    { 
+        name: 'Global Industries', 
+        location: 'Miami' },
+    { 
+        name: 'Bright Future Enterprises', 
+        location: 'Dallas' },
+    { 
+        name: 'Innovative Solutions', 
+        location: 'San Francisco' },
+    { 
+        name: 'MegaCorp', 
+        location: 'Boston' },
+    { 
+        name: 'PrimeTime Corporation', 
+        location: 'Atlanta' },
+    { 
+        name: 'Northern Lights Co.', 
+        location: 'Seattle' },
+    { 
+        name: 'TechWise Solutions', 
+        location: 'Austin' },
+    { 
+        name: 'SkyHigh Enterprises', 
+        location: 'Denver' },
+    { 
+        name: 'Dynamic Industries', 
+        location: 'Washington D.C.' },
+    { 
+        name: 'Growth Company', 
+        location: 'Houston' },
+    { 
+        name: 'Pinnacle Inc.', 
+        location: 'Philadelphia' },
+    { 
+        name: 'Leading Edge Solutions', 
+        location: 'San Diego' },
+    { 
+        name: 'Summit Enterprises', 
+        location: 'Minneapolis' },
+    { 
+        name: 'Elite Industries', 
+        location: 'Phoenix' },
+    { 
+        name: 'Golden Gate Corporation', 
+        location: 'San Francisco' },
+    { 
+        name: 'Optimum Solutions', 
+        location: 'Orlando' },
+    { 
+        name: 'Bayside Corporation', 
+        location: 'Tampa' },
+    { 
+        name: 'Silver State Enterprises', 
+        location: 'Las Vegas' },
+    { 
+        name: 'First Rate Co.', 
+        location: 'New York' },
+    { 
+        name: 'Sunset Corporation', 
+        location: 'Los Angeles' },
+    { 
+        name: 'Infinite Solutions', 
+        location: 'Chicago' },
+    { 
+        name: 'East Coast Enterprises', 
+        location: 'Miami' },
+    { 
+        name: 'Rising Star Co.', 
+        location: 'Dallas' },
+    { 
+        name: 'NextGen Industries', 
+        location: 'San Francisco' },
+    { 
+        name: 'Peak Performance Corporation', 
+        location: 'Boston' },
+    { 
+        name: 'New Horizons Solutions', 
+        location: 'Atlanta' }
   ];
 
   export default companies_list;
