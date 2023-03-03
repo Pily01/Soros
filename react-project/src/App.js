56cb2e77bdaa985eb18a9b443933b57235fdc8ab
Arsamat <67019552+Arsamat@users.noreply.github.com>
2023-03-03 12:04:26 -0500

    Created a form with firebase upload

--- react-project/src/App.js
+++ react-project/src/App.js
@@ -4,12 +4,14 @@ import './App.css';
 
 import Home from './routes/home/home.component';
 import Company from './components/company/company.component';
+import FormInput from './components/form-input/form-input.component'
 
 const App = () => {
   return (
     <Routes>
       <Route path='/Soros' element={<Home/>}/>
       <Route path='/Soros/companies/:name' element={<Company/>}/>
+      <Route path='/Soros/form' element={<FormInput/>}/>
     </Routes>
     
   );
