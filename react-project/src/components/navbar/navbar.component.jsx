001893545bf742a42255c5fdd098df70907839c5
Dominguez <pilymdm@bu.edu>
2023-03-12 16:13:59 -0600

    feat: remove home link from navbar

--- react-project/src/components/navbar/navbar.component.jsx
+++ react-project/src/components/navbar/navbar.component.jsx
@@ -9,7 +9,6 @@ function NavbarComponent() {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
             <Nav.Link href="#link">About Us</Nav.Link>
           </Nav>
         </Navbar.Collapse>
