aa25e0feb2d18c7ee380e5405adf544dd81a7e17
Dominguez <pilymdm@bu.edu>
2023-03-12 19:15:52 -0600

    refactor: implement find company and use data

--- react-project/src/components/company/company.component.jsx
+++ react-project/src/components/company/company.component.jsx
@@ -6,27 +6,13 @@ const Company = () => {
     const {name} = useParams();
     console.log(useParams())
 
+    const foundCompany = companies_list.find(company => company.name === name)
+
     return(
         <div> 
-            <h1> Company: {name}</h1>
-            <h2> 3.7 / 5 </h2>
-            <h2> 60% of workers say the feel safe here </h2>
-            <h3> Gender distribution: 30% women 70%</h3>
-            <h3> Reported sexual abuse </h3>
-            <ul>
-                <li>Unwanted physical contact</li>
-                <li>Text messages with sexual content</li>
-                <li>Threats</li>
-                <li>Comments on clothes and body</li>
-            </ul>
-            <h3> 74% of harassers are in a higher possition of power</h3>
-            <h3>Reported Departments</h3>
-            <ul>
-                <li>Marketing</li>
-                <li>Accounting</li>
-                <li>Computer Systems</li>
-            </ul>
-            
+            <h1> {foundCompany.name}</h1>
+            <h2> {foundCompany.rating} / 5 </h2>
+            <p> {foundCompany.location} total reviews</p> 
         </div>
     )
 }
