3088864d6cbc67f125508f4ff9af788099f8f1af
Dominguez <pilymdm@bu.edu>
2023-03-12 19:15:33 -0600

    feat: add rating of companies

--- react-project/src/data/companies.js
+++ react-project/src/data/companies.js
@@ -1,67 +1,106 @@
 let companies_list = [
     { 
         name: 'Teleperformance', 
-        location: '1 review' },
+        location: '1',
+        rating:'3'
+    },
     { 
         name: 'Sliders', 
-        location: '1 Review' },
+        location: '1',
+        rating:'3'
+    },
     { 
         name: 'Secretaria de A.F', 
-        location: '1 Review' },
+        location: '1 Review',
+        rating:'4'
+    },
     { 
         name: 'CMF GAM', 
-        location: '1 Review' },
+        location: '1',
+        rating:'4'
+    },
     { 
         name: 'Starbucks Centro Historico', 
-        location: '1 Review' },
+        location: '1',
+        rating:'5'
+    },
     { 
         name: 'Hospital Español', 
-        location: '1 Review' },
+        location: '1',
+        rating:'4'
+    },
     { 
         name: 'BBVA', 
-        location: '1 Review' },
+        location: '1',
+        rating:'4'
+    },
     { 
         name: 'Banorte', 
-        location: '1 Review' },
+        location: '1',
+        rating:'4'
+    },
     { 
         name: 'Rhenus Torre Mitikah', 
-        location: '1 Review' },
+        location: '1',
+        rating: '4'
+    },
     { 
         name: 'Hospital General de Mexico', 
-        location: '1 Review' },
+        location: '1',
+        rating: '2'},
     { 
         name: 'Escuela Moderna Americana', 
-        location: '1 Review' },
+        location: '1',
+        rating: '3'},
     { 
         name: 'Aeroméxico', 
-        location: '1 Review' },
+        location: '1',
+        rating: '3'},
     { 
         name: 'Pipiol Camp', 
-        location: '1 Review' },
+        location: '1',
+        rating: '5'},
     { 
         name: 'INDICE', 
-        location: '1 Review' },
+        location: '1',
+        rating: '5'},
     { 
         name: 'Millesime', 
-        location: '1 Review' },
+        location: '1',
+        rating: '4'},
     { 
         name: 'Alsea', 
-        location: '1 Review' },
+        location: '1',
+        rating: '4'},
     { 
         name: 'AGVM', 
-        location: '1 Review' },
+        location: '1',
+        rating: '3'},
     { 
         name: 'Medica Mia', 
-        location: '1 Review' },
+        location: '1',
+        rating:'3'
+    },
     { 
         name: 'Nápoles', 
-        location: '1 Review' },
+        location: '1',
+        rating:'5'
+    },
     { 
         name: 'Svetia', 
-        location: '1 Review' },
+        location: '1',
+        rating:'5'
+    },
     { 
         name: 'Instituto Mexicano del Petróleo', 
-        location: '1 Review' },
+        location: '1',
+        rating:'4'
+    },
+    { 
+        name: 'deep_dive', 
+        location: '1',
+        rating:'5'
+    },
   ];
 
   export default companies_list;
