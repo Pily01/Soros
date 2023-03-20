4a10b8ca8ecc162761696d5038864c7a88c3218e
Dominguez <pilymdm@bu.edu>
2023-03-20 16:55:33 -0600

    feat: update component according to high fidelity wireframe

--- react-project/src/routes/home/home.component.jsx
+++ react-project/src/routes/home/home.component.jsx
@@ -1,12 +1,13 @@
 import '../../App.css';
 import './home.styles.scss'
+import homeimg from '../../soros-home.png'
+
 import SearchBox from '../../components/search/search-box';
 import CardList from '../../components/cardlist/cardlist';
 import NavbarComponent from '../../components/navbar/navbar.component';
 import {useState, useEffect} from 'react';
-import FormInput from "../.././components/form-input/form-input.component";
 
-import {Button, Container, Row} from 'react-bootstrap';
+import {Container, Row, Col} from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 
@@ -36,14 +37,17 @@ const Home = () => {
     <div className='App-main-div'>
       <NavbarComponent/>
       <Container fluid>
-        <Row className='home-jumbotron'>
-          <h1> SOROS</h1>
-          <h3> Empowering Women: One Safe Place At A Time</h3>
+        <Row >
+          <Col sm={8} className='home-jumbotron-col1'>
+            <p>Together, we can make workplaces in Mexico City safer.</p>
+            <h1> Prevent, report, and check <br/>anonymously</h1>
+            <SearchBox onChangeHandler={onSearch}/>
+          </Col>
+          <Col sm={4} className='home-jumbotron-col2'>
+           <img className="home-img" src={homeimg} alt="home img"/>
+          </Col>
         </Row>
         <Row className='home-search-bar'>
-          <h1> Prevent, report, and check </h1>
-          <h1> anonymously </h1>
-          <SearchBox onChangeHandler={onSearch}/>
           <CardList filteredList={filtList}/>
         </Row>
       </Container>
