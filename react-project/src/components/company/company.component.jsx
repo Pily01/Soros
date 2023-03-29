ca74eb4f6407724ac239a5f00008d49d50735092
Dominguez <pilymdm@bu.edu>
2023-03-29 14:39:13 -0600

    feat: add grid layout to component

--- react-project/src/components/company/company.component.jsx
+++ react-project/src/components/company/company.component.jsx
@@ -1,11 +1,11 @@
 import { useParams } from "react-router-dom"
 import companies_list from "../../data/companies";
 import { Link } from 'react-router-dom';
-import { Button } from 'react-bootstrap';
 import { useState, useEffect, useRef } from 'react';
 import { collection, query, where, getDocs, Firestore } from "firebase/firestore";
 import { db } from '../../utils/firebase/firebase.utils';
 
+import { Button, Container, Row, Col } from 'react-bootstrap';
 
 const Company = () => {
     const { name } = useParams();
@@ -39,26 +39,36 @@ const Company = () => {
 
     console.log(companyData)
 
-    const foundCompany = companies_list.find(company => company.name === name)
-
     return (
         <div>
-            <Link className="cardlist-link" to={new_href}>
-                <Button className='cardlist-button'>Report</Button>
-            </Link>
-            <h1> {name}</h1>
+            
             {companyData.map(company => {
                 return (
-                    <div>
-                        <h2>{company.rating}</h2>
-                        <p>Adress: {company.address}</p>
-                        <p>Company Support: {company.companySupport}</p>
-                        <p>Experienced Harassment: {company.experiencedHarassment}</p>
-                        <p>Frequency: {company.frequency}</p>
-                        <p>Reported Harassment: {company.reportedHarassment}</p>
-                        <p>Witnessed Harassment: {company.witnessedHarassment}</p>
-                        <p>Safe: {company.safe}</p>
-                    </div>
+                    <Container>
+                        <Row>
+                            <Col>
+                                <h1> {name}</h1>
+                                <h1>{company.rating}/5</h1>
+                                <Link className="cardlist-link" to={new_href}>
+                                    <Button className='cardlist-button'>Rate</Button>
+                                </Link>
+                            </Col>
+                            <Col>
+                                Address: {company.address}
+                                Website: https://examplewebsite.com
+                            </Col>
+                        </Row>
+                    </Container>
+                    // <div>
+                    //     <h2>{company.rating}</h2>
+                    //     <p>Adress: {company.address}</p>
+                    //     <p>Company Support: {company.companySupport}</p>
+                    //     <p>Experienced Harassment: {company.experiencedHarassment}</p>
+                    //     <p>Frequency: {company.frequency}</p>
+                    //     <p>Reported Harassment: {company.reportedHarassment}</p>
+                    //     <p>Witnessed Harassment: {company.witnessedHarassment}</p>
+                    //     <p>Safe: {company.safe}</p>
+                    // </div>
 
                 )
             })}
