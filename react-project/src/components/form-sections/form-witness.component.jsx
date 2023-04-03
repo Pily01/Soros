66ed59d393ccce61ae04e6fcd81ac8e7f8441a56
Dominguez <pilymdm@bu.edu>
2023-04-02 19:30:14 -0600

    feat: add witness component

---
+++ react-project/src/components/form-sections/form-witness.component.jsx
@@ -0,0 +1,42 @@
+import { Form } from 'react-bootstrap';
+
+const FormWitness = () => {
+    return (
+        <div>
+            {/* Witness */}
+            <Form.Group className="mb-3" controlId="witnessed">
+                <Form.Label>Have you witnessed any form of sexual harassment at this company? </Form.Label>
+                {['radio'].map((type) => (
+                    <div key={`default-${type}`} className="mb-3">
+                        <Form.Check type={type} label= 'Yes' name="witnessed" 
+                        value="Yes" 
+                        />
+                        <Form.Check type={type} label= 'No' name="witnessed" 
+                        value="No"
+                        />
+                    </div>
+                ))}
+            </Form.Group>
+            
+            {/* Frequency */}
+            <Form.Group className="mb-3" controlId="frequency">
+                <Form.Label>If yes, how often did you witness the harassment? </Form.Label>
+                {['radio'].map((type) => (
+                    <div key={`default-${type}`} className="mb-3">
+                        <Form.Check type={type} label= 'Rare' name="frequent"
+                        value="rarely" 
+                        />
+                        <Form.Check type={type} label= 'Often' name="frequent" 
+                        value="often"
+                        />
+                        <Form.Check type={type} label= 'Very frequent' name="frequent" 
+                        value="very often"
+                        />
+                    </div>
+                ))}
+            </Form.Group>
+        </div>
+    )
+}
+
+export default FormWitness;
