890752bd586a0fdb814ed3517ee094d75cfae053
Dominguez <pilymdm@bu.edu>
2023-04-02 22:26:58 -0600

    fix: make  rating component questions required

--- react-project/src/components/form-sections/form-rating.component.jsx
+++ react-project/src/components/form-sections/form-rating.component.jsx
@@ -10,15 +10,15 @@ const FormRating = () => {
                 <Form.Label>On a scale from 1 to 5, how would you rate this company? *</Form.Label>
                 {['radio'].map((type) => (
                     <div key={`inline-${type}`} className="mb-3">
-                        <Form.Check inline type={type} label= '1' name="rating" value="1" 
+                        <Form.Check inline type={type} label= '1' name="rating" value="1" required 
                         />
-                        <Form.Check inline type={type} label= '2' name="rating" value="2"
+                        <Form.Check inline type={type} label= '2' name="rating" value="2" required
                         />
-                        <Form.Check inline type={type} label= '3' name="rating" value="3"
+                        <Form.Check inline type={type} label= '3' name="rating" value="3" required
                         />
-                        <Form.Check inline type={type} label= '4' name="rating" value="4"
+                        <Form.Check inline type={type} label= '4' name="rating" value="4" required
                         />
-                        <Form.Check inline type={type} label= '5' name="rating" value="5"
+                        <Form.Check inline type={type} label= '5' name="rating" value="5" required
                         />
                     </div>
                 ))}
@@ -29,14 +29,14 @@ const FormRating = () => {
                 <Form.Label>2. How safe do you feel at work? *</Form.Label>
                 {['radio'].map((type) => (
                     <div key={`inline-${type}`} className="mb-3">
-                        <Form.Check inline type={type} label= 'Not safe at all' name="safe"
+                        <Form.Check inline type={type} label= 'Not safe at all' name="safe" required
                         value = "not safe"
                         />
-                        <Form.Check inline type={type} label= 'Moderately safe' name="safe"
+                        <Form.Check inline type={type} label= 'Moderately safe' name="safe" required
                         value = "moderately safe"
                         
                         />
-                        <Form.Check inline type={type} label= 'Completely safe' name="safe"
+                        <Form.Check inline type={type} label= 'Completely safe' name="safe" required
                         value ="super safe"
     
                         />
