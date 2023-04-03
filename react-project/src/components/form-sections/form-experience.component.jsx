0447e846896f3008eb8c4ab645e629352a5058b1
Dominguez <pilymdm@bu.edu>
2023-04-02 19:21:06 -0600

    feat: add experience section component

---
+++ react-project/src/components/form-sections/form-experience.component.jsx
@@ -0,0 +1,42 @@
+import { Form } from 'react-bootstrap';
+
+const FormExperience = () => {
+    return(
+        <div>
+            {/* Experience */}
+            <Form.Group className="mb-3" controlId="experience">
+                <Form.Label>Have you experienced any form of sexual harassment at this workplace?</Form.Label>
+                {['radio'].map((type) => (
+                    <div key={`default-${type}`} className="mb-3">
+                        <Form.Check type={type} label= 'Yes' name="experience" 
+                        value="Yes" 
+                        />
+                        <Form.Check type={type} label= 'No' name="experience" 
+                        value="No"
+                        />
+                    </div>
+                ))}
+            </Form.Group >
+
+            {/* Frequency */}
+            <Form.Group className="mb-3" controlId="frequency">
+                <Form.Label>If yes, how frequent was the harassment? </Form.Label>
+                {['radio'].map((type) => (
+                    <div key={`default-${type}`} className="mb-3">
+                        <Form.Check type={type} label= 'Rare' name="frequent"
+                        value="rarely" 
+                        />
+                        <Form.Check type={type} label= 'Often' name="frequent" 
+                        value="often"
+                        />
+                        <Form.Check type={type} label= 'Very frequent' name="frequent" 
+                        value="very often"
+                        />
+                    </div>
+                ))}
+            </Form.Group>
+        </div>
+    )
+}
+
+export default FormExperience;
