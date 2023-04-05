1b2f38dc36ad5e814ecea87874449466efc9e9cb
Dominguez <pilymdm@bu.edu>
2023-04-05 12:30:46 -0600

    feat: implement FormContext in component

--- react-project/src/components/multi-page-form/form-company-details.component.jsx
+++ react-project/src/components/multi-page-form/form-company-details.component.jsx
@@ -1,16 +1,26 @@
 import { Form } from 'react-bootstrap';
 
-const FormCompanyDetails = () => {
+import FormContext from '../../context/form.context';
 
+const FormCompanyDetails = () => {
+    const { data, handleChange } = FormContext;
     return(
         <Form>
             {/* Company Name */}
             <Form.Group className="mb-3" controlId="company">
                 <Form.Label>What is the name of the company you would like to report?*</Form.Label>
-                <Form.Control required type="text" className="mb-3" 
+                <Form.Control className="mb-3"
+                required 
+                type="text"
+                value={data.companyName}
+                onChange={handleChange}
                 />
                 <Form.Label>What is the address?*</Form.Label>
-                <Form.Control required type="text" className="mb-3" 
+                <Form.Control required 
+                type="text" 
+                className="mb-3"
+                value={data.companyAdress}
+                onChange={handleChange}
                 />
             </Form.Group >
         </Form>
