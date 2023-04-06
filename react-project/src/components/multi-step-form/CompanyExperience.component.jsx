6edef983513a18e653b0d5bd561547919dad5249
Dominguez <pilymdm@bu.edu>
2023-04-06 15:24:28 -0600

    feat: finish layout of company experience component

--- react-project/src/components/multi-step-form/CompanyExperience.component.jsx
+++ react-project/src/components/multi-step-form/CompanyExperience.component.jsx
@@ -1,9 +1,79 @@
 import React from 'react';
 
-const CompanyExperience = () => {
+const CompanyExperience = ({formData, setFormData}) => {
+    const {experiencedHarass, experiencedFrequency} = formData;
+
     return (
         <div>
+            <label htmlFor="harassment">Have you experienced any form of sexual harassment at this company?</label><br/>
+            <input 
+            type="radio" 
+            id="harassment-yes" 
+            name="harassment" 
+            value="yes" 
+            onChange={(event) =>
+                setFormData({ ...formData, experiencedHarass: event.target.value })
+            }
+            checked = {experiencedHarass == "yes"}
+            />
+            <label htmlFor="harassment-yes">Yes</label><br/>
+            <input 
+            type="radio" 
+            id="harassment-no" 
+            name="harassment" 
+            value="no"
+            onChange={(event) =>
+                setFormData({ ...formData, experiencedHarass: event.target.value })
+            }
+            checked = {experiencedHarass == "no"}
+            />
+            <label htmlFor="harassment-no">No</label><br/>
 
+
+            <label>If yes, how frequent was the harassment?</label><br/>
+            <input type="radio" 
+            name="frequency" 
+            id="rare" 
+            value="rare" 
+            onChange={(event) =>
+                setFormData({ ...formData, experiencedFrequency: event.target.value })
+            }
+            checked = {experiencedFrequency == "rare"}
+            />
+            <label htmlFor="rare">Rare</label><br/>
+
+            <input type="radio" 
+            name="frequency" 
+            id="sometimes" 
+            value="sometimes" 
+            onChange={(event) =>
+                setFormData({ ...formData, experiencedFrequency: event.target.value })
+            }
+            checked = {experiencedFrequency == "sometimes"}
+            />
+            <label htmlFor="sometimes">Sometimes</label><br/>
+
+            <input type="radio" 
+            name="frequency" 
+            id="often" 
+            value="often" 
+            onChange={(event) =>
+                setFormData({ ...formData, experiencedFrequency: event.target.value })
+            }
+            checked = {experiencedFrequency == "often"}
+            />
+            <label for="often">Often</label><br/>
+
+            <input type="radio" 
+            name="frequency" 
+            id="always" 
+            value="always" 
+            onChange={(event) =>
+                setFormData({ ...formData, experiencedFrequency: event.target.value })
+            }
+            checked = {experiencedFrequency == "always"}
+            />
+            <label for="always">Always</label><br/>
         </div>
     )
 }
