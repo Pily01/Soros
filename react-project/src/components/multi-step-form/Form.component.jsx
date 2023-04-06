93b3d7d39081f8d782212b2c7b0350faa74d194d
Dominguez <pilymdm@bu.edu>
2023-04-06 13:56:33 -0600

    feat: add formData and titles

--- react-project/src/components/multi-step-form/Form.component.jsx
+++ react-project/src/components/multi-step-form/Form.component.jsx
@@ -1,6 +1,22 @@
 import React, {useState} from 'react';
 
 const Form = () => {
+    const [page, setPage] = useState(0);
+    const[formData, setFormData] = useState({
+        companyName: "",
+        companyAdress: "",
+        rating: "",
+        safety: "",
+        experiencedHarass: "",
+        experiencedFrequency: "",
+        witnessedHarass: "",
+        witnessedFrequency: "",
+        reportedHarass: "",
+        support: ""
+    })
+
+    const FormTitles = ["Company Details", "Rating", "Experience", "Witnessed", "Report"];
+
     return (
         <div>
 
