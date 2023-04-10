1602c6b0d8c0c32a95adbddd75db194bfe42927c
Arsamat <67019552+Arsamat@users.noreply.github.com>
2023-04-09 23:29:13 -0400

    Added data aggregation for firestore

--- react-project/src/components/multi-step-form/Form.component.jsx
+++ react-project/src/components/multi-step-form/Form.component.jsx
@@ -5,6 +5,9 @@ import CompanyExperience from './CompanyExperience.component';
 import CompanyWitness from './CompanyWitness.component';
 import CompanyReport from './CompanyReport.component';
 
+import { doc, getDoc, setDoc, updateDoc, increment} from "firebase/firestore";
+import {db} from '../../utils/firebase/firebase.utils';
+
 const Form = () => {
     const [page, setPage] = useState(0);
     const[formData, setFormData] = useState({
@@ -21,6 +24,46 @@ const Form = () => {
     })
 
     const FormTitles = ["Company Details", "Rating", "Experience", "Witnessed", "Report"];
+
+
+    const handleData = async () => {
+        const docRef = doc(db, "companies", formData.companyName);
+        const docSnap = await getDoc(docRef);
+
+        if(docSnap.exists()){
+            console.log("Exists");
+            await updateData(docRef);
+        }
+        else{
+            await setDoc(docRef, {
+                name: formData.companyName,
+                address: formData.companyAddress,
+                rating: {"1": 0, "2": 0, "3": 0},
+                experiencedHarass: {"yes": 0, "no": 0},
+                experiencedFrequency: {"rare": 0, "sometimes": 0, "often": 0, "always": 0},
+                safety: {"safe": 0, "moderately safe": 0, "unsafe": 0},
+                witnessedHarass: {"yes": 0, "no": 0},
+                witnessedFrequency: {"rare": 0, "sometimes": 0, "often": 0, "always": 0},
+                reportedHarass: {"yes": 0, "no": 0},
+                support: {"fully-resolved": 0, "partially-resolved": 0, "no-action": 0}
+            });
+            console.log("Doesn't exist")
+            await updateData(docRef);
+        }
+    }
+
+    const updateData = async (docRef) => {
+        await updateDoc(docRef, {
+            [`rating.${formData.rating}`]: increment(1),
+            [`experiencedHarass.${formData.experiencedHarass}`]: increment(1),
+            [`experiencedFrequency.${formData.experiencedFrequency}`]: increment(1),
+            [`safety.${formData.safety}`]: increment(1),
+            [`witnessedHarass.${formData.witnessedHarass}`]: increment(1),
+            [`witnessedFrequency.${formData.witnessedFrequency}`]: increment(1),
+            [`reportedHarass.${formData.reportedHarass}`]: increment(1),
+            [`support.${formData.support}`]: increment(1)
+        })
+    }
 
     const pageDisplay = () => {
         if(page === 0){
@@ -65,11 +108,11 @@ const Form = () => {
                         if (page === FormTitles.length - 1) {
                           alert("FORM SUBMITTED");
                           console.log(formData);
+                          handleData();
                         } else {
                           setPage((currPage) => currPage + 1);
                         }
-                      }}
-                >
+                      }}>
                     {page === FormTitles.length - 1 ? "Submit" : "Next"}
                 </button>
             </div>
