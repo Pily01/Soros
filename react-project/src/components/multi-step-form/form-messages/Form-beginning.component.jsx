43aa46b8a4abddd80c335e400b4cc0c147634939
Dominguez <pilymdm@bu.edu>
2023-04-13 20:56:17 -0600

    styles: add image and finish figma styles to component

--- react-project/src/components/multi-step-form/form-messages/Form-beginning.component.jsx
+++ react-project/src/components/multi-step-form/form-messages/Form-beginning.component.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import NavbarComponent from '../../navbar/navbar.component';
+import begginingImg from '../../../form-beginning-img.png'
 
 import './Form-beginnig.styles.scss'
 
@@ -9,12 +10,13 @@ const FormBeginning = () => {
         <div>
             <NavbarComponent/>
             <div className='beginning-container'>
-            <h1>Your privacy is our priority.</h1>
-            <p>The following form will not ask for any self-identifying information about <br/> 
-            yourself. Please answer the questions to the best of your ability.</p>
-            <a href='/Soros/multi-form'><button> Start Safety Report Form </button></a>
-            <a className='link' href=''>What is this form?</a>
-        </div>
+                <img className='form-img' src={begginingImg} alt="img" />
+                <h1>Your privacy is our priority.</h1>
+                <p>The following form will not ask for any self-identifying information about <br/> 
+                yourself. Please answer the questions to the best of your ability.</p>
+                <a href='/Soros/multi-form'><button> Start Safety Report Form </button></a>
+                <a className='link' href=''>What is this form?</a>
+            </div>
         </div>
         
     );
