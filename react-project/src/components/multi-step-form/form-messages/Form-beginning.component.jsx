127f13efe0fbfac2fc30d28ecede0062f481c4ba
Dominguez <pilymdm@bu.edu>
2023-04-13 18:28:12 -0600

    feat: add Navbar component

--- react-project/src/components/multi-step-form/form-messages/Form-beginning.component.jsx
+++ react-project/src/components/multi-step-form/form-messages/Form-beginning.component.jsx
@@ -1,16 +1,22 @@
 import React from 'react';
 
+import NavbarComponent from '../../navbar/navbar.component';
+
 import './Form-beginnig.styles.scss'
 
 const FormBeginning = () => {
     return (
-        <div className='beginning-container'>
+        <div>
+            <NavbarComponent/>
+            <div className='beginning-container'>
             <h1>Your privacy is our priority.</h1>
             <p>The following form will not ask for any self-identifying information about <br/> 
             yourself. Please answer the questions to the best of your ability.</p>
-            <button> Start Safety Report Form </button>
-            <a className='link' href='/form'>What is this form?</a>
+            <a href='/Soros/multi-form'><button> Start Safety Report Form </button></a>
+            <a className='link' href=''>What is this form?</a>
         </div>
+        </div>
+        
     );
 }
 
