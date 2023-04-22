f57e0dfb6eb51ab16ab48c29d0c1ee33dd8cb30d
Dominguez <pilymdm@bu.edu>
2023-04-21 19:47:06 -0600

    refactor: save each company key-value data separately

--- react-project/src/components/company/company.component.jsx
+++ react-project/src/components/company/company.component.jsx
@@ -1,143 +1,143 @@
 import { useParams } from "react-router-dom"
-import { Link } from 'react-router-dom';
 import { useState, useEffect, useRef } from 'react';
-
 import { collection, query, where, getDocs, Firestore } from "firebase/firestore";
 import { db } from '../../utils/firebase/firebase.utils';
 
-import { Button, Container, Row, Col, Table } from 'react-bootstrap';
 import "./company.styles.scss"
 
-import Example from "./company-safe-chart.component";
+import SafeChart from "./company-safe-chart.component";
 
 const Company = () => {
     const { name } = useParams();
-    console.log(name);
     //change the url for the next redirect to form
-    const href = window.location.href;
-    const new_href = href.replace("companies", "form");
 
-    const [companyData, setCompanyData] = useState([]);
-    const effectRef = useRef(false);
-    const tempCompanyData = []
-    const [myMap, setMyMap] = useState(new Map());
-
-    const updateMap = (k, v) => {
-        setMyMap(new Map(myMap.set(k, v)));
-    }
-
-    // useEffect(() => {
-    //     const queryGet = async () => {
-    //         const reviewsRef = collection(db, "companies");
-    //         const q = query(reviewsRef, where("companyName", "==", `${name}`));
-
-    //         const querySnapshot = await getDocs(q);
-    //         querySnapshot.forEach((doc) => {
-    //             // doc.data() is never undefined for query doc snapshots
-    //             //console.log(doc.id, " => ", doc.data())
-    //             tempCompanyData.push(doc.data());
-    //         });
-    //         setCompanyData(tempCompanyData)
-    //     }
-    //     if(effectRef.current) return
-    //     effectRef.current = true;
-    //     queryGet();
-    // }, [])
+    const [address, setAddress] = useState("")
+    const [rating, setRating] = useState({});
+    const [safety, setSafety] = useState({});
+    const [experiencedFrequency, setExperiencedFrequency] = useState({});
+    const [experiencedHarass, setExperiencedHarass] = useState({});
+    const [witnessedHarass, setWitnessedHarass] = useState({});
+    const [witnessedFrequency, setWitnessedFrequency] = useState({});
+    const [support, setSupport] = useState({});
 
     useEffect(() => {
-        const queryGet = async () => {
-            const q = query(collection(db, "companies"), where("companyName", "==", `${name}`));
-            const querySnapshot = await getDocs(q);
-            querySnapshot.forEach((doc) => {
-                // doc.data() is never undefined for query doc snapshots
-                const data = doc.data()
-                for (const key in data) {
-                    if (key !== "address" && key !== "companyName") {
-                        updateMap(key, data[key])
-                    }
-                }
-            }
-            )
+      const queryGet = async () => {
+        const q = query(
+          collection(db, "companies_fake"),
+          where("companyName", "==", `${name}`)
+        );
+        const querySnapshot = await getDocs(q);
+        querySnapshot.forEach((doc) => {
+          const data = doc.data();
+
+          setAddress(data.address || "");
+          setRating(data.rating || {});
+          setSafety(data.safety || {});
+          setExperiencedHarass(data.experiencedHarass || {});
+          setExperiencedFrequency(data.experiencedFrequency || {});
+          setWitnessedHarass(data.witnessedHarass || {});
+          setWitnessedFrequency(data.witnessedFrequency || {});
+          setSupport(data.support || {});
+
+        });
+      };
+
+      queryGet();
+    }, [name]);
+
+    console.log("DATA ---------------------");
+    console.log(address); // USED
+    console.log(rating);  // USED
+    console.log(safety);
+    console.log(experiencedHarass);
+    console.log(experiencedFrequency);
+    console.log(witnessedHarass);
+    console.log(witnessedFrequency);
+    console.log(support);
+
+    const getRating = () => {
+        let totalRating = 0;
+        let totalVotes = 0;
+        for (const [key, value] of Object.entries(rating)) {
+            totalRating += parseInt(key) * parseInt(value);
+            totalVotes += parseInt(value);
         }
+        return totalVotes === 0 ? 0 : (totalRating / totalVotes).toFixed(1);
+    }
 
-        if (effectRef.current) return;
-        effectRef.current = true;
-        queryGet();
-    }, [])
-
-    console.log(myMap)
+    const getVotes = () => {
+        let votes = 0;
+        for (const [key, value] of Object.entries(rating)) {
+            votes += parseInt(value);
+        }
+        return votes
+    }
 
     return (
         <div>
-
-            {companyData.map(company => {
-                return (
-                    <Container className="company-container">
-                        <Row className="company-main-row">
-                            <Col className="company-main-info">
-                                <h1 className="company-name"> {name}</h1>
-                                <h1 className="rating">{company.rating}/5</h1>
-                                <p>Overall Quality Based on 1 rating</p>
-                                <Link className="cardlist-link" to={new_href}>
-                                    <Button className='cardlist-button' size="lg">Rate</Button>
-                                </Link>
-                            </Col>
-                            <Col className="company-secondary-info">
-                                <Table borderless>
-                                    <tbody>
-                                        <tr>
-                                            <td><b>Address:</b></td>
-                                            <td>{company.address}</td>
-                                        </tr>
-                                        <tr>
-                                            <td><b>Website:</b></td>
-                                            <td>https://www.example.com</td>
-                                        </tr>
-                                    </tbody>
-                                </Table>
-                            </Col>
-                        </Row>
-                        <Row>
-                            <Col className="harassment-cases">
-                                <p><b>100%</b> of users experienced hasrrassment</p>
-                                <p><b>0%</b> of reported cases were resolved</p>
-                            </Col>
-                            <Col>
-                                <h4>Overall Safety</h4>
-                                <Example />
-                            </Col>
-                        </Row>
-                    </Container>
-                    // <div>
-                    //     <h2>{company.rating}</h2>
-                    //     <p>Adress: {company.address}</p>
-                    //     <p>Company Support: {company.companySupport}</p>
-                    //     <p>Experienced Harassment: {company.experiencedHarassment}</p>
-                    //     <p>Frequency: {company.frequency}</p>
-                    //     <p>Reported Harassment: {company.reportedHarassment}</p>
-                    //     <p>Witnessed Harassment: {company.witnessedHarassment}</p>
-                    //     <p>Safe: {company.safe}</p>
-                    // </div>
-                )
-            })}
-
-            <div>
-                {
-                    Array.from(myMap, ([key1, innerMap]) => (
-                        <div key={key1}>
-                            <h2>{key1}</h2>
-                            {Array.from(Object.entries(innerMap), ([key2, value2]) => (
-                                <p key={key2}>
-                                    {key2}: {value2}
-                                </p>
-                            ))}
-                        </div>
-                    ))
-                }
+            <div className="title">
+                <h2>{name}</h2>
+                <p>{address}</p>
             </div>
-
+            <div className="rating">
+                <p>Score generated by {getVotes()} reports</p>
+                <h1>{getRating()}</h1>
+            </div>
+            <SafeChart charData={safety}/>
         </div>
     )
 }
 
 export default Company
+
+
+// {companyData.map(company => {
+//     return (
+//         <Container className="company-container">
+//             <Row className="company-main-row">
+//                 <Col className="company-main-info">
+//                     <h1 className="company-name"> {name}</h1>
+//                     <h1 className="rating">{company.rating}/5</h1>
+//                     <p>Overall Quality Based on 1 rating</p>
+//                     <Link className="cardlist-link" to={new_href}>
+//                         <Button className='cardlist-button' size="lg">Rate</Button>
+//                     </Link>
+//                 </Col>
+//                 <Col className="company-secondary-info">
+//                     <Table borderless>
+//                         <tbody>
+//                             <tr>
+//                                 <td><b>Address:</b></td>
+//                                 <td>{company.address}</td>
+//                             </tr>
+//                             <tr>
+//                                 <td><b>Website:</b></td>
+//                                 <td>https://www.example.com</td>
+//                             </tr>
+//                         </tbody>
+//                     </Table>
+//                 </Col>
+//             </Row>
+//             <Row>
+//                 <Col className="harassment-cases">
+//                     <p><b>100%</b> of users experienced hasrrassment</p>
+//                     <p><b>0%</b> of reported cases were resolved</p>
+//                 </Col>
+//                 <Col>
+//                     <h4>Overall Safety</h4>
+//                     <Example />
+//                 </Col>
+//             </Row>
+//         </Container>
+//         // <div>
+//         //     <h2>{company.rating}</h2>
+//         //     <p>Adress: {company.address}</p>
+//         //     <p>Company Support: {company.companySupport}</p>
+//         //     <p>Experienced Harassment: {company.experiencedHarassment}</p>
+//         //     <p>Frequency: {company.frequency}</p>
+//         //     <p>Reported Harassment: {company.reportedHarassment}</p>
+//         //     <p>Witnessed Harassment: {company.witnessedHarassment}</p>
+//         //     <p>Safe: {company.safe}</p>
+//         // </div>
+//     )
+// })}
