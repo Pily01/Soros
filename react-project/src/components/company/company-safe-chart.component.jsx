5885e56450afd3bfa52fb1b9bf8290a1b043b6f5
Dominguez <pilymdm@bu.edu>
2023-04-22 23:39:42 -0600

    feat: finish company-safe chart component

--- react-project/src/components/company/company-safe-chart.component.jsx
+++ react-project/src/components/company/company-safe-chart.component.jsx
@@ -1,24 +1,26 @@
 import React, { PureComponent } from 'react';
 import { BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
 
-const data = [
-  {
-    name: 'Completely safe',
-    uv: 0,
-  },
-  {
-    name: 'Moderately Safe',
-    uv: 100,
-  },
-  {
-    name: 'Not safe',
-    uv: 0,
-  }
-];
 
-const SafeChart = (chartData) => {
-    console.log(chartData)
-    
+const SafeChart = ({ safety }) => {
+    console.log(safety["safe"]);
+    const total = safety["safe"] + safety["unsafe"] + safety["moderately safe"];
+
+
+    const data = [
+      {
+        name: 'Completely safe',
+        uv: ((safety["safe"] / total) * 100),
+      },
+      {
+        name: 'Moderately Safe',
+        uv: ((safety["moderately safe"] / total) * 100),
+      },
+      {
+        name: 'Not safe',
+        uv: ((safety["unsafe"] / total) * 100),
+      }
+    ];
 
     return (
       <ResponsiveContainer width="80%" height="80%" aspect={1}>
@@ -37,12 +39,10 @@ const SafeChart = (chartData) => {
           <XAxis dataKey="name" />
           <YAxis />
           <Tooltip />
-          <Legend />
           <Bar dataKey="uv" fill="#9381FF" />
         </BarChart>
       </ResponsiveContainer>
     );
-  
 }
 
 export default SafeChart
