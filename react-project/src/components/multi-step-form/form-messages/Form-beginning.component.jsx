38b9b023e774534a0a10046f452d8d00a372ee96
Dominguez <pilymdm@bu.edu>
2023-04-24 11:40:01 -0600

    feat: add icons and new section to component

--- react-project/src/components/multi-step-form/form-messages/Form-beginning.component.jsx
+++ react-project/src/components/multi-step-form/form-messages/Form-beginning.component.jsx
@@ -11,6 +11,7 @@ const FormBeginning = () => {
             <div className='beginning-container'>
                 <img className='form-img' src={begginingImg} alt="img" />
                 <h1>Your privacy is our priority.</h1>
+                <p><i class="fa-regular fa-clock"></i> 2 - 5 min <i class="fa-regular fa-circle-question"></i> 9 questions <i class="fa-solid fa-user-secret"></i> 100% anonymous</p>
                 <p>The following form will not ask for any self-identifying information about <br/> 
                 yourself. Please answer the questions to the best of your ability.</p>
                 <a href='/Soros/multi-form'><button> Start Safety Report Form </button></a>
