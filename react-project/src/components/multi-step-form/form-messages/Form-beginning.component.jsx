dee6d3923d30fb41ff361cc7275dbbb6d303a77d
Dominguez <pilymdm@bu.edu>
2023-04-24 14:22:55 -0600

    feat: add link to resources

--- react-project/src/components/multi-step-form/form-messages/Form-beginning.component.jsx
+++ react-project/src/components/multi-step-form/form-messages/Form-beginning.component.jsx
@@ -19,7 +19,7 @@ const FormBeginning = () => {
                 <p>The following form will not ask for any self-identifying information about <br/> 
                 yourself. Please answer the questions to the best of your ability.</p>
                 <a href='/Soros/multi-form'><button> Start Safety Report Form </button></a>
-                <a className='link' href=''>What is this form?</a>
+                <a className='link' href='/Soros/resources'>What is this form?</a>
                 <div className='disclaimer-section'>
                     <p><i class="fa-solid fa-triangle-exclamation"></i> This form asks optional questions about sexual assault, harassment, and other experiences that may be traumatic to some users.</p>
                 </div>
