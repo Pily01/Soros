93d43e363355036b5de0c6da576dfa18266e46e4
Dominguez <pilymdm@bu.edu>
2023-04-24 11:53:32 -0600

    styles: add new figma styles to beggining form component

--- react-project/src/components/multi-step-form/form-messages/Form-beginning.component.jsx
+++ react-project/src/components/multi-step-form/form-messages/Form-beginning.component.jsx
@@ -11,11 +11,18 @@ const FormBeginning = () => {
             <div className='beginning-container'>
                 <img className='form-img' src={begginingImg} alt="img" />
                 <h1>Your privacy is our priority.</h1>
-                <p><i class="fa-regular fa-clock"></i> 2 - 5 min <i class="fa-regular fa-circle-question"></i> 9 questions <i class="fa-solid fa-user-secret"></i> 100% anonymous</p>
+                <div className='icon-section'>
+                    <p><i class="fa-regular fa-clock"></i> 2 - 5 min </p> 
+                    <p><i class="fa-regular fa-circle-question"></i> 9 questions</p>
+                    <p><i class="fa-solid fa-user-secret"></i> 100% anonymous</p>
+                </div>
                 <p>The following form will not ask for any self-identifying information about <br/> 
                 yourself. Please answer the questions to the best of your ability.</p>
                 <a href='/Soros/multi-form'><button> Start Safety Report Form </button></a>
                 <a className='link' href=''>What is this form?</a>
+                <div className='disclaimer-section'>
+                    <p><i class="fa-solid fa-triangle-exclamation"></i> This form asks optional questions about sexual assault, harassment, and other experiences that may be traumatic to some users.</p>
+                </div>
             </div>
         </div>
         
