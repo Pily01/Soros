67f5b2bc277a97a2d0d9d5602141b2693ab6b968
Dominguez <pilymdm@bu.edu>
2023-04-29 22:54:03 -0600

    feat: wrap input in input group to add input group textext

--- react-project/src/components/search/search-box.jsx
+++ react-project/src/components/search/search-box.jsx
@@ -1,15 +1,23 @@
 import { Component } from "react";
 import "./search-box.styles.scss"
 
-import { Form } from 'react-bootstrap'
+import { Form, InputGroup } from 'react-bootstrap'
 
 //search box componenet that will monitor change in user's search string
 const SearchBox = props => {
 
     return (
-            <Form>
-                <Form.Control className="search-search-bar" type="search" placeholder="Search Company" onChange={props.onChangeHandler} />
-            </Form>
+        <Form>
+            <InputGroup className="mb-3 input-group-search">
+                 <InputGroup.Text id="basic-addon1"><i class="fa-solid fa-magnifying-glass"></i></InputGroup.Text>
+                <Form.Control 
+                className="search-search-bar"
+                aria-describedby="basic-addon1"
+                type="search" 
+                placeholder="Search Company" 
+                onChange={props.onChangeHandler} />
+            </InputGroup>
+        </Form>
     )
 }
 
