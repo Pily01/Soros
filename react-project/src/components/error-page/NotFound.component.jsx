e5bf016dcf17375dd2b3f87ae9842f6d0409ca2a
Dominguez <pilymdm@bu.edu>
2023-05-01 11:31:35 -0600

    feat: created 404 error page

---
+++ react-project/src/components/error-page/NotFound.component.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Container } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import "./NotFound.styles.scss"
+
+const NotFound = () => {
+  return (
+          <Container className="PageContainer">
+              <h1>404</h1>
+              <h2 id="mainWriting">Page Not Found</h2>
+              <p id="description">Something went wrong! Please return to home page</p>
+              <Link className="home-link" to='/Soros'>Go to Home </Link>
+          </Container>
+        );
+    }
+
+export default NotFound;
