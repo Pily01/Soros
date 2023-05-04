da2cc87dc3d2b4f92fb40d1ecd19e93d78e67af6
Dominguez <pilymdm@bu.edu>
2023-05-03 22:23:06 -0600

    styles: make the form-message component responsive

--- react-project/src/components/multi-step-form/form-messages/Form-end.component.jsx
+++ react-project/src/components/multi-step-form/form-messages/Form-end.component.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 
-import NavbarComponent from '../../navbar/navbar.component';
+import begginingImg from '../../../form-beginning-img.png'
 import './Form-beginnig.styles.scss'
 
 const FormEnd = () => {
   return (
     <div>
             <div className='beginning-container'>
+                <img className='form-img' src={begginingImg} alt="img" />
                 <h1>Thank you for making a difference.</h1>
                 <p>We have successfully received your response. Your<br/>review will be up in one to two days.</p>
                 <a href='/Soros'><button> Back to Home </button></a>
