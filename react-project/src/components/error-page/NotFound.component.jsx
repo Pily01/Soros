1693b1719a02f5555167d2ed3fd88bb464528782
Dominguez <pilymdm@bu.edu>
2023-05-04 13:57:21 -0600

    docs: add documentation to not found component

--- react-project/src/components/error-page/NotFound.component.jsx
+++ react-project/src/components/error-page/NotFound.component.jsx
@@ -1,12 +1,16 @@
+// ---------------  N O T  F O U N D  C O M P O N E N T ---------------//
+// Component to display when 404 ERROR
+
 import React from 'react';
 import { useRef, useEffect } from 'react';
-import { Container } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
+// - Styles
+import { Container } from 'react-bootstrap';
 import "./NotFound.styles.scss"
 
 const NotFound = () => {
   const effectRef = useRef(false);
-
+  // Google translate element
   const googleTranslateElementInit = (callback) => {
     new window.google.translate.TranslateElement(
       {
@@ -17,12 +21,11 @@ const NotFound = () => {
       },
       "google_translate_element"
     );
-
     if (typeof callback === 'function') {
       callback();
     }
   };
-
+  // Google translate use Effect
   useEffect(() => {
     const gTranslate = () => {
       var addScript = document.createElement("script");
@@ -33,26 +36,23 @@ const NotFound = () => {
       document.body.appendChild(addScript);
       window.googleTranslateElementInit = googleTranslateElementInit;
     }
-    
     if(effectRef.current) return
     effectRef.current = true;
     gTranslate();
-
   }, []);
 
-
   return (
-          <div>
-          <div id="google_translate_element">
-          </div>
-          <Container className="PageContainer">
-              <h1>404</h1>
-              <h2 id="mainWriting">Page Not Found</h2>
-              <p id="description">Something went wrong! Please return to home page</p>
-              <Link className="home-link" to='/Soros'>Go to Home </Link>
-          </Container>
-          </div>
-        );
-    }
+    <div>
+    <div id="google_translate_element">
+    </div>
+    <Container className="PageContainer">
+        <h1>404</h1>
+        <h2 id="mainWriting">Page Not Found</h2>
+        <p id="description">Something went wrong! Please return to home page</p>
+        <Link className="home-link" to='/Soros'>Go to Home </Link>
+    </Container>
+    </div>
+  );
+}
 
 export default NotFound;
