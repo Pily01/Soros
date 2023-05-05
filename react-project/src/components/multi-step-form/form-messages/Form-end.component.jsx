227168af1f88940efc8ac4764be0aafa13860b7a
Dominguez <pilymdm@bu.edu>
2023-05-04 23:24:37 -0600

    feat: update end form message

--- react-project/src/components/multi-step-form/form-messages/Form-end.component.jsx
+++ react-project/src/components/multi-step-form/form-messages/Form-end.component.jsx
@@ -46,7 +46,7 @@ const FormEnd = () => {
         <div className='beginning-container'>
             <img className='form-img' src={begginingImg} alt="img" />
             <h1>Thank you for making a difference.</h1>
-            <p>We have successfully received your response. Your<br/>review will be up in one to two days.</p>
+            <p>We have successfully received your response.</p>
             <a href='/Soros'><button> Back to Home </button></a>
         </div>
     </div>
